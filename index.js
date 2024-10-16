0b64554fbfcc02af4df83131401a46788c407dd3
AdityaGarg1165 <90836787+AdityaGarg1165@users.noreply.github.com>
2024-10-16 23:19:50 +0530

    added link functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path")
 const http = require('http');
 const fs = require('fs');
 const { Server } = require('socket.io');
@@ -8,18 +9,65 @@ const server = http.createServer(app);
 const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
+
 app.use(express.static('public'));
 
-// Middleware to parse JSON requests
+
 app.use(express.json());
 
-// Route to get public IP address
+// Routes
 app.get('/', (req, res) => {
     const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     console.log(`Incoming request from IP: ${ipAddress}`);
     res.send(`Your public IP address is: ${ipAddress}`);
 });
+
+
+
+app.get('/download/:filename', (req, res) => {
+    const { filename } = req.params;
+    const filePath = path.join(__dirname, filename); // Use the filename from the URL
+
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.error('File not found:', filename);
+            return res.status(404).send('File not found.');
+        }
+        
+        // Serve the file for download
+        res.download(filePath, (err) => {
+            if (err) {
+                console.error('Error downloading the file:', err);
+                res.status(500).send('Error downloading the file.');
+            } else {
+                // Delete the file after download is complete
+                fs.unlink(filePath, (err) => {
+                    if (err) {
+                        console.error(`Error deleting file: ${filename}`, err);
+                    } else {
+                        console.log(`File ${filename} was deleted successfully after download.`);
+                    }
+                });
+            }
+        });
+    });
+});
+
+
+app.get('/link', (req, res) => {
+    const filePath = path.join(__dirname, 'public', 'link.html');
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.error('Error sending file:', err);
+            res.status(err.status).end();
+        } else {
+            console.log('link.html sent successfully');
+        }
+    });
+});
+
+
 
 const fileChunks = {};
 const users = {};
@@ -45,7 +93,15 @@ io.on('connection', (socket) => {
         
     }
 
-    console.log(users);
+    socket.on("remove",()=>{
+        users[ipAddress] = users[ipAddress].filter(x=>x !== socket.id)
+    })
+
+    socket.on('fileComplete', (data) => {
+        socket.emit('uploadSuccess');
+    });
+
+    socket.emit("selfId",socket.id)
 
     socket.join(ipAddress);
 
@@ -71,8 +127,7 @@ io.on('connection', (socket) => {
             fileChunks[fileName].push(Buffer.from(chunkData));
             const progress = ((currentChunk + 1) / totalChunks) * 100;
             socket.emit('progress', { fileName, progress });
-
-            // Emit the chunk to other users
+            
             users[ipAddress].forEach(x => {
                 if (x !== socket.id) {
                     io.to(x).emit("receiveChunks", {
@@ -84,13 +139,13 @@ io.on('connection', (socket) => {
                     });
                 }
             });
-
+            
             console.log(`Received chunk ${currentChunk + 1}/${totalChunks} for file ${fileName}`);
         } else {
-            // Handle finalizing the file upload
+            
             const fileBuffer = Buffer.concat(fileChunks[fileName]);
             const savedFilePath = `./uploads/${fileName}`;
-
+            
             fs.writeFile(savedFilePath, fileBuffer, (err) => {
                 if (err) {
                     console.error(`Error saving file: ${err}`);
@@ -99,12 +154,123 @@ io.on('connection', (socket) => {
                     io.emit('fileSaved', { fileName });
                 }
             });
-
-            // Clear file chunks for the next upload
+    
+            
             delete fileChunks[fileName];
         }
     });
+    
+    const linkFileChunks = {}
+    
+    socket.on("linkchunks",(data)=>{
+        const { fileName, fileType, currentChunk, totalChunks, chunkData } = data;
+        if(!linkFileChunks[fileName]){
+            
+            linkFileChunks[fileName] = []
+        }
+        console.log(data.code)
+        
+        linkFileChunks[fileName].push(Buffer.from(chunkData))
+        
+        if(chunkData === "uc"){
+            const fileBuffer = Buffer.concat(linkFileChunks[fileName])
+            
+
+            fs.writeFile(socket.id + fileName, fileBuffer, (err) => {
+                delete fileChunks[fileName];
+            });
+
+        
+        }
+        
+
+        
+    })
+
+
+const CHUNK_SIZE = 695450
+socket.on("requestFile",(data)=>{
+    const filePath = path.join(__dirname, '3_Idiots.mp4');
+    const fileStat = fs.statSync(filePath);
+    const totalChunks = Math.ceil(fileStat.size / CHUNK_SIZE);
+    let currentChunk = 0;
+    const sendChunk = () => {
+        const start = currentChunk * CHUNK_SIZE;
+        const end = Math.min(start + CHUNK_SIZE, fileStat.size);
+        const readStream = fs.createReadStream(filePath, { start, end: end - 1 });
+
+        readStream.on('data', (chunk) => {
+            socket.emit('requestedChunk', {
+                fileName:"sdf.mp4",
+                totalChunks,
+                currentChunk,
+                chunk: chunk.toString('base64')
+            });
+        });
+
+        readStream.on('end', () => {
+            currentChunk++;
+            if (currentChunk < totalChunks) {
+                sendChunk();
+            }
+        });
+    };
+
+    sendChunk();
+})
+
+
 });
+
+const deleteOldFiles = () => {
+    const directoryPath = path.join(__dirname); // Directory to check for old files
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            console.error('Error reading directory:', err);
+            return;
+        }
+
+        const now = Date.now();
+        const twentyFourHours = 60 * 1000; // 24 hours in milliseconds
+
+        files.forEach((file) => {
+
+            const filePath = path.join(directoryPath, file);
+            
+
+            // Exclude .js and .json files from deletion
+            if (file.endsWith('.js') || file.endsWith('.json') || file.endsWith('.lock') || file.endsWith('.html')) {
+                console.log(`Skipping deletion of file: ${file}`);
+                return;
+            }
+
+            fs.stat(filePath, (err, stats) => {
+                if (err) {
+                    console.error('Error getting file stats:', err);
+                    return;
+                }
+
+                const fileAge = now - stats.mtimeMs; // Time since last modified
+                if (fileAge > twentyFourHours) {
+                    fs.unlink(filePath, (err) => {
+                        if (err) {
+                            console.error('Error deleting file:', err);
+                        } else {
+                            console.log(`Deleted old file: ${file}`);
+                        }
+                    });
+                }
+            });
+        });
+    });
+};
+
+// Schedule file deletion every hour
+setInterval(deleteOldFiles,60 * 1000); // Check every hour
+
+
+
+
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
